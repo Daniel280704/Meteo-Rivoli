300c37f4b58cc7c33a521d01f2cab33bd6c2a135
Daniel <154078145+Daniel280704@users.noreply.github.com>
2026-07-13 14:14:16 +0200

    Update aggiornamento_meteo_quotidiano.py

--- aggiornamento_meteo_quotidiano.py
+++ aggiornamento_meteo_quotidiano.py
@@ -138,7 +138,7 @@ def interpella_gemini(dati_testuali, oggi_str, domani_str):
     """
     try:
         response = client.models.generate_content(
-            model='gemini-flash-latest',
+            model='gemini-flash-lite-latest',
             contents=prompt,
             config=types.GenerateContentConfig(temperature=0.25)
         )
